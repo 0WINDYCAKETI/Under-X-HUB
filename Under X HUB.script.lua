8c1ccb1ffce685e7d96f0d4d0bee389d4258ec5e
0WINDYCAKETI <112848663+0WINDYCAKETI@users.noreply.github.com>
2023-03-29 18:54:59 +0700

    Update Under X HUB.script.lua

--- Under X HUB.script.lua
+++ Under X HUB.script.lua
@@ -1,1 +1,11 @@
+-- Under X HUB --
 
+local delay = 0 -- / [Thời gian] nếu như wifi của bạn quá yếu
+
+repeat wait() wait(delay) until game:IsLoaded()
+
+getgenv().JoinTeam = "Pirate" -- / Hải Tạc
+
+shared.keys = "6a3918e1b0350f925d3fe91493499d9" -- / Chìa Khóa
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0WINDYCAKETI/Scripts/main/IDK.lua"))()
